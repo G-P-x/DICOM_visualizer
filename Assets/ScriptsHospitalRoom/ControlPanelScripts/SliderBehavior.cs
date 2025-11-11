f6394e58f0620d47c69ebcd9f26cfb53f1009cb2
G-P-x <g.pau15@studenti.unica.it>
2025-11-11 22:04:03 +0100

    Update SliderBehavior to initialize fillRectImage and change slider color dynamically based on value

--- Assets/ScriptsHospitalRoom/ControlPanelScripts/SliderBehavior.cs
+++ Assets/ScriptsHospitalRoom/ControlPanelScripts/SliderBehavior.cs
@@ -13,9 +13,15 @@ public class SliderBehavior : MonoBehaviour
     private SliderType sliderType = SliderType.MIN_VALUE;
     public SlidersManager manager; // Reference to the SlidersManager
     public Slider slider; // Reference to the Slider component
+    private Image fillRectImage;
 
     void Start()
     {
+        fillRectImage = slider.fillRect.GetComponent<Image>();
+        if (fillRectImage == null)
+        {
+            Debug.LogError("SliderBehavior: No Image component found in children for background.");
+        }
         if (slider == null)
         {
             slider = GetComponent<Slider>();
@@ -30,12 +36,14 @@ public class SliderBehavior : MonoBehaviour
             slider.minValue = 0f;
             slider.maxValue = 1f;
             slider.value = 0f; // Initialize to minimum
+            fillRectImage.color = Color.cyan;
         }
         else if (sliderType == SliderType.MAX_VALUE)
         {
             slider.minValue = 0f;
             slider.maxValue = 1f;
             slider.value = 1f; // Initialize to maximum
+            fillRectImage.color = Color.red;
         }
     }
 
@@ -50,18 +58,24 @@ public class SliderBehavior : MonoBehaviour
     }
     private void ChangeColor()
     {
-        // Example method to change the color of the slider based on its value
-        Image fill = slider.fillRect.GetComponent<Image>();
-        if (fill != null)
+        if (slider.value <= 0.33f)
         {
-            if (slider.value < slider.maxValue / 2)
-            {
-                fill.color = Color.green;
-            }
-            else
-            {
-                fill.color = Color.red;
-            }
+            float localValue = slider.value/0.33f;
+            fillRectImage.color = Color.Lerp(Color.cyan, Color.green, localValue);
+        }
+        else if (slider.value > 0.33f && slider.value <= 0.66f)
+        {
+            float localValue = (slider.value - 0.33f)/0.33f;
+            fillRectImage.color = Color.Lerp(Color.green, Color.yellow, localValue);
+        }
+        else if (slider.value > 0.66f && slider.value < 1f)
+        {
+            float localValue = (slider.value - 0.66f)/0.34f;
+            fillRectImage.color = Color.Lerp(Color.yellow, Color.red, localValue);
+        }
+        else
+        {
+            fillRectImage.color = Color.red;
         }
         return;
     }
